e86ffa2e33fa3871dcbb39dd6e6f16d643820176
André <Andr@150.162.208.71>
2018-07-24 20:43:01 -0300

    New distance function

---
+++ msm-move/src/main/java/br/ufsc/lehmann/ProportionDistance.java
@@ -0,0 +1,28 @@
+package br.ufsc.lehmann;
+
+import br.ufsc.core.trajectory.IDistanceFunction;
+
+public class ProportionDistance implements IDistanceFunction<Number> {
+	
+	@Override
+	public double distance(Number p, Number d) {
+		if(p == d) {
+			return 0;
+		}
+		if (p == null || d == null) {
+			return Double.MAX_VALUE;
+		}
+		return 1 - (Math.min(p.doubleValue(), d.doubleValue()) / Math.max(p.doubleValue(), d.doubleValue()));
+	}
+
+	@Override
+	public double convert(double units) {
+		return units;
+	}
+	
+	@Override
+	public double maxDistance() {
+		return Double.MAX_VALUE;
+	}
+
+}
